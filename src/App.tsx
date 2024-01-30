1818fb17c1d555e14fd50e1b0cc30d383e95a122
Tuomo Kivinen <mrtuomok@gmail.com>
2024-01-30 11:02:58 +0200

    add hds-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import { LoadingSpinner, Card } from "hds-react";
+import { Login } from "./Login";
+import { Cookies } from "./Cookies";
 
 function App() {
   return (
     <div className="App">
       <LoadingSpinner />
       <Card title="title" heading="heading" text="content text" border />
+      <Cookies />
+      <Login />
     </div>
   );
 }
